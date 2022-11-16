bfb60d261a4e116e8873116888eb63b2f8a43eb1
Fabio Sousa <clubeteczagem@gmail.com>
2022-11-16 13:01:01 -0300

    atualização do sistema

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
     "net/http"
 )
 
-var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
+var addr = flag.String("addr", ":8000", "http service address") // Q=17, R=18
 
 var templ = template.Must(template.New("qr").Parse(templateStr))
 
@@ -21,26 +21,39 @@ func main() {
 }
 
 func QR(w http.ResponseWriter, req *http.Request) {
-    templ.Execute(w, req.FormValue("s"))
+    templ.Execute(w, req.FormValue("input_formulario_qr_code"))
 }
 
 const templateStr = `
 <html>
 <head>
-<title>QR Link Generator</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<title>Gerador de qr code</title>
 </head>
+
+
+
 <body>
+<div class="d-flex  justify-content-center rounded-3">
+<div class="card  d-flex " style="width: 22rem;">
 {{if .}}
 <img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
-<br>
-{{.}}
-<br>
-<br>
-{{end}}
-<form action="/" name=f method="POST">
-    <input maxLength=1024 size=70 name=s value="" title="Text to QR Encode">
-    <input type=submit value="Show QR" name=qr>
+  <div class="card-body">
+    {{end}}
+    <h5 class="fs-6">{{.}}</h5>
+    <form class="d-flex flex-column  justify-content-center" action="/" name=f method="POST">  
+
+    <input class="mb-4"  name=s value="" title="Text to QR Encode">
+   
+    <input class="btn btn-primary "type=submit value="Gerar QR" name=qr>
 </form>
+</div>    
+  </div>
+</div>
+
+
+
 </body>
 </html>
 `
